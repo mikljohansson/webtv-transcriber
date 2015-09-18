c81908540cc63fef1a779c32a1fee855722a31e2
Mikael Johansson <mikael.johansson@meltwater.com>
2015-09-18 11:20:49 +0200

    Functional download/transcode

--- crawler/webtv/pipelines.py
+++ crawler/webtv/pipelines.py
@@ -1,35 +1,37 @@
-import os, subprocess, tempfile, hashlib
+import os, subprocess, tempfile, hashlib, logging
+from scrapy.exceptions import DropItem
 
 def shell(cmd):
-    return subprocess.call(cmd, shell=True)
+    logging.info('Running ' + ' '.join(cmd))
+    return subprocess.call(cmd)
 
 def store(url, src):
-	digest = hashlib.md5.new(url).hexdigest()
-	directory = '/data/' + digest[0:2] + '/' + digest[2:6] + '/' + digest[6:] + '/'
-	os.path.mkdirs(directory)
-	os.rename(src, os.path.join(directory, os.path.basename(src)))
+    digest = hashlib.md5.new(url).hexdigest()
+    directory = '/data/' + digest[0:2] + '/' + digest[2:6] + '/' + digest[6:] + '/'
+    os.path.mkdirs(directory)
+    os.rename(src, os.path.join(directory, os.path.basename(src)))
 
 class SVTPlayDownload(object):
     def process_item(self, item, spider):
-		outputdir = tempfile.mkdtemp()
-		try:
-			outputbase = os.path.join(outputdir, 'output')
-			audiotrack = outputbase+'.mp3'
-			subtitles = outputbase+'.srt'
-			
-			code = shell(['svtplay-dl', '-o', outputbase, '-f', '-S', '-q 1', '-Q', '100000', item['url']])
-			if code != 0:
-				raise DropItem('Failed to download video from %s' % item['url'])
-			if not os.path.exists(subtitles):
-				raise DropItem('Video did not contain subtitles %s' % item['url'])
-			
-			code = shell(['ffmpeg', '-i', outputbase+'.ts', '-y', '-vn', '-f', 'mp3', '-ac', '1', '-q:a', '6', audiotrack])
-			if code != 0:
-				raise DropItem('Failed to extract audio from %s' % item['url'])
+        outputdir = str(tempfile.mkdtemp())
+        try:
+            outputbase = os.path.join(outputdir, 'output')
+            audiotrack = outputbase+'.mp3'
+            subtitles = outputbase+'.srt'
+            
+            code = shell(['svtplay-dl', '-o', outputbase, '-f', '-S', '-q', '1', '-Q', '10000', item['url']])
+            if code != 0:
+                raise DropItem('Failed to download video from %s' % item['url'])
+            if not os.path.exists(subtitles):
+                raise DropItem('Video did not contain subtitles %s' % item['url'])
+            
+            code = shell(['ffmpeg', '-i', outputbase+'.ts', '-y', '-vn', '-f', 'mp3', '-ac', '1', '-q:a', '6', audiotrack])
+            if code != 0:
+                raise DropItem('Failed to extract audio from %s' % item['url'])
 
-			store(url, audiotrack)
-			store(url, subtitles)
-	    finally:
-	    	shell(['rm' '-rf', outputdir])
+            store(url, audiotrack)
+            store(url, subtitles)
+        finally:
+            shell(['rm', '-rf', outputdir])
 
         return item
--- crawler/webtv/settings.py
+++ crawler/webtv/settings.py
@@ -4,4 +4,4 @@ SPIDER_MODULES = ['webtv.spiders']
 NEWSPIDER_MODULE = 'webtv.spiders'
 DEFAULT_ITEM_CLASS = 'webtv.items.Video'
 
-ITEM_PIPELINES = {'dirbot.pipelines.RabbitMQSink': 1}
+ITEM_PIPELINES = {'webtv.pipelines.SVTPlayDownload': 1}
