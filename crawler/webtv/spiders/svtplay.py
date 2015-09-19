13604f61bb8a5e01317876759631e27afd012515
Mikael Johansson <mikl.johansson@gmail.com>
2015-09-19 11:58:32 +0200

    Strip query string from svtplay.se url to avoid downloading the same video multiple times

--- crawler/webtv/spiders/svtplay.py
+++ crawler/webtv/spiders/svtplay.py
@@ -1,9 +1,16 @@
 import re, logging
+from urlparse import urlparse, urlunparse
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
 from scrapy.linkextractors import LinkExtractor
 
 from webtv.items import Video
+
+def stripurl(url):
+    """
+    Strip trailing ;parameters?query=123#fragment from url to normalize it
+    """
+    return urlunparse(urlparse(url)[0:3] + (None,None,None)).rstrip('/')
 
 class SVTPlaySpider(CrawlSpider):
     name = 'svtplay'
@@ -22,6 +29,6 @@ class SVTPlaySpider(CrawlSpider):
         sel = Selector(response)
         item = Video()
         item['name'] = sel.xpath('//head/title/text()').extract()
-        item['url'] = response.url
+        item['url'] = stripurl(response.url)
         logging.info("Video '%s' at '%s'", item['name'], item['url'])
         return item
